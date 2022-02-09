d27c56171178e5c26ed6f2d98a02933a97d86a22
Breanna Shields <bree@Breannas-MacBook-Pro.local>
2022-02-09 19:05:15 +1000

    hamburger menu

---
+++ main.js
@@ -0,0 +1,21 @@
+<html> · 
+    
+    <head> · 
+        <script type="text/javascript" src="message.js"></script> · 
+        
+    </head> · 
+    
+    <body> · 
+    
+        function myFunction() {
+        var x = document.getElementById("myLinks");
+        if (x.style.display === "block") {
+            x.style.display = "none";
+        } else {
+            x.style.display = "block";
+        }
+    }
+
+    </body>
+
+</html>
